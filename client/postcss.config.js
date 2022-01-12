d9f992945631e1740a2b33b8499978f35814811e
Kerrin Gillis <kegillis118@gmail.com>
2022-01-12 10:35:12 -0500

    Initial commit

---
+++ client/postcss.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+module.exports = {
+  plugins: [
+    require("postcss-preset-env"),
+    require("postcss-nesting")(),
+    require("postcss-assets")(),
+    require("postcss-import")(),
+    require("tailwindcss")(`${__dirname}/tailwind.config.js`),
+    require("autoprefixer"),
+  ],
+};
