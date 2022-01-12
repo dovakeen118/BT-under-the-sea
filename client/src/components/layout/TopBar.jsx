6a1d741966acbd433b46e3d2d51a55ff38a472f1
dovakeen118 <kegillis118@gmail.com>
2022-01-12 18:20:09 -0500

    top bar and base styling

--- client/src/components/layout/TopBar.jsx
+++ client/src/components/layout/TopBar.jsx
@@ -2,17 +2,37 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import "../../style/layouts/topBar.pcss";
+
 export function TopBar() {
   return (
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <ul className="menu">
-          <li className="menu-text">App</li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
+    <nav id="top-bar">
+      <div className="top-bar-home">
+        <svg
+          className="top-bar-icon"
+          width="54"
+          height="54"
+          viewBox="0 0 54 54"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+        </svg>
+        <span className="top-bar-title">Under the Sea</span>
       </div>
-    </div>
+
+      <div id="top-bar-navigation">
+        <div className="top-bar-links">
+          <Link to="/" className="top-bar-link">
+            Home
+          </Link>
+        </div>
+
+        <div>
+          <a href="#sign-up" className="top-bar-user">
+            Sign up
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 /* eslint-disable global-require */
 module.exports = {
   plugins: [require("@tailwindcss/forms")],
+  theme: {
+    colors: {
+      current: "currentColor",
+      transparent: "transparent",
+      white: colors.white,
+      sage: "#d3dacd",
+      teal: "#3C7882",
+    },
+  },
 };
